bccf7d0266034ed312a5a4f0228ff6c48bce85ad
Ali Nazar <ali_nazar91@hotmail.se>
2019-12-01 21:58:28 +0100

    added LazyDecorator example

---
+++ AdaptiveCodeTraining/Lazy decorators/LazyComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AdaptiveCodeTraining.DecoratorPattern;
+
+namespace AdaptiveCodeTraining.Lazydecorators
+{
+    public class LazyComponent : IComponent
+    {
+        public Lazy<IComponent> _LazyComponent { get; }
+
+        public LazyComponent(Lazy<IComponent> lazyComponent)
+        {
+            _LazyComponent = lazyComponent;
+        }
+
+        public void Something()
+        {
+            _LazyComponent.Value.Something();
+        }
+    }
+
+    // using lazycomponent
+
+    public class ComponentClient
+    {
+        public IComponent _Component { get; }
+
+        public ComponentClient(IComponent component)
+        {
+            _Component = component;
+        }
+
+        public void Run()
+        {
+            _Component.Something();
+        }
+    }
+}
---
+++ AdaptiveCodeTraining/Lazy decorators/README.md
@@ -0,0 +1,1 @@
+The lazy decorator allows clients to be provided with a reference to an interface that will not be instantiated until its first use.
